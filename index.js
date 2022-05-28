fe7eed47ded9e19d8ff3e5e38f3b1ada5bd8fa4f
script.sh <script.sh.me@gmail.com>
2022-05-28 12:06:42 +0300

    changed

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ const host = '127.0.0.1';
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/registration.html'); 
+    res.sendFile(__dirname + '/public/login.html'); 
 });
 
-app.post('/login', (req, res) => {
-    res.sendFile(__dirname + '/public/login.html');
+app.get('/registration', (req, res) => {
+    res.sendFile(__dirname + '/public/registration.html');
+});
+
+app.get('/tap_to_play', (req, res) => {
+    res.sendFile(__dirname + '/public/tap_to_play.html');
+});
+
+app.get('/play', (req, res) => {
+    res.sendFile(__dirname + '/public/play.html');
 });
 
 app.listen(process.env.PORT || port, host, () => {
